eab648266ce6f959255c2549e6b002fdb428ec62
imbenham <isaacbenham@Isaacs-MacBook-Pro.local>
2015-04-16 20:30:17 -0400

    view controller set up
    Added some IBOutlet stubs and functions I know we’ll need

---
+++ MidtermCalculator/CalculatorLogic.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorLogic.swift
+//  MidtermCalculator
+//
+//  Created by Isaac Benham on 4/16/15.
+//  Copyright (c) 2015 Sarah Peltier. All rights reserved.
+//
+
+import Foundation
+
+enum Operation: Int {
+    case Plus = 0
+    case Minus = 1
+    case Multiply = 2
+    case Divide = 4
+}
--- MidtermCalculator/ViewController.swift
+++ MidtermCalculator/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var inputString = "0"
+    var currentOperation:Operation? = nil
+    var currentValue:Float = 0
+    var inputValue:Float? = nil
+    
+    @IBAction func handleClearTap(sender: UIButton) {
+    
+    }
+    @IBAction func handlePlusMinusTap(sender: UIButton) {
+    
+    }
+    @IBAction func handlePercentButtonTap(sender: UIButton) {
+    
+    }
+    @IBAction func handleOperationButtonTap(sender: UIButton) {
+        currentOperation = Operation(rawValue: sender.tag)
+        sender.layer.borderWidth = 5
+    }
+    @IBAction func handleNumericButtonTap(sender: UIButton) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +41,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updatStringWithInput(input: String) {
+        if currentValue == 0 {
+            inputString = "\(input)"
+        } else {
+            inputString += "\(input)"
+        }
     }
 
 
