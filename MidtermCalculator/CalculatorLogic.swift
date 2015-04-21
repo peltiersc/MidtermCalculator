2db7418ccba705c2e42a3a54bbdbae0252646358
imbenham <isaacbenham@Isaacs-MacBook-Pro.local>
2015-04-21 19:23:10 -0400

    changed a bunch of stuff
    mainly to accommodate order of operations, but also to handle user
    input logic. The good news is all the IB connections remain the same,
    so we should be able to merge our work.

--- MidtermCalculator/CalculatorLogic.swift
+++ MidtermCalculator/CalculatorLogic.swift
@@ -13,4 +13,36 @@ enum Operation: Int {
     case Subtract = 1
     case Multiply = 2
     case Divide = 3
+    
+    func evaluateOperation(#firstValue: Float, secondValue:Float) -> Float{
+        switch self{
+        case .Plus:
+            return firstValue + secondValue
+        case .Subtract:
+            return firstValue - secondValue
+        case .Multiply:
+            return firstValue * secondValue
+        case .Divide:
+            return firstValue / secondValue
+        }
+    }
+}
+typealias Node = (Operation, Float)
+
+func walkTree(var nodes:[Node], initial:Float) -> Float {
+    
+    for node in nodes{
+        switch node.0 {
+        case .Plus:
+            nodes.removeAtIndex(0)
+            return initial + walkTree(nodes, node.1)
+        case .Subtract:
+            nodes.removeAtIndex(0)
+            return initial - walkTree(nodes, node.1)
+        default:
+            nodes.removeAtIndex(0)
+            return walkTree(nodes, node.0.evaluateOperation(firstValue: initial, secondValue:node.1))
+        }
+    }
+    return initial
 }
