a199ba8d058b7c87ff340e6ab33258f50dac74a8
imbenham <isaacbenham@Isaacs-MacBook-Pro.local>
2015-04-21 22:35:52 -0400

    finished all logic except for toggling OoO

--- MidtermCalculator/ViewController.swift
+++ MidtermCalculator/ViewController.swift
@@ -24,21 +24,50 @@ class ViewController: UIViewController {
     @IBAction func handleClearTap(sender: UIButton) {
         if clearFlag {
             inputValue = nil
-            currentValue = 0
+            currentValue = nil
+            clearFlag = false
+            inputString = "0"
         } else {
             inputValue = nil
+            inputString = "0"
+            clearFlag = true
         }
         
-        clearFlag = !clearFlag
+        
+    }
+    
+    @IBAction func handleDecimalButtonTap(sender: UIButton) {
+        decimalFlag = true
+        if decimalCount == 0 {
+            decimalCount++
+        }
     }
     
     @IBAction func handlePlusMinusTap(sender: UIButton) {
-    
+        if currentValueIsDisplayed() {
+            currentValue! -= currentValue! * 2
+            updateDisplayWithCurrent()
+        } else {
+            if inputValue != nil {
+                inputValue! -= inputValue! * 2
+                updateDisplayWithInput()
+            }
+        }
     }
     @IBAction func handlePercentButtonTap(sender: UIButton) {
-    
+        if currentValueIsDisplayed() {
+            currentValue! /= 100
+            updateDisplayWithCurrent()
+        } else {
+            if inputValue != nil {
+                inputValue! /= 100
+                updateDisplayWithInput()
+            }
+        }
     }
     @IBAction func handleOperationButtonTap(sender: UIButton) {
+        decimalFlag = false
+        decimalCount = 0
         let newOp = Operation(rawValue: sender.tag)!
         if currentOperation != nil {
             if inputValue != nil {
@@ -81,17 +110,24 @@ class ViewController: UIViewController {
     }
     
     func updateInputValueAndString(input: Int) {
+        println("update called")
         if inputValue == 0 || inputValue == nil {
             inputValue = Float(input)
             inputString = "\(input)"
+            println("inputVal: \(inputValue)")
         } else {
-            inputString += "\(input)"
-            let intify = inputString.toInt()!
-            inputValue = Float(intify)
+            self.formatInputString("\(input)")
+            self.formatInputValue()
+            println("inputString: \(inputString)")
+            println("inputVal: \(inputValue)")
         }
     }
     
     func evaluateCurrentExpression() {
+        if currentValue == nil {
+            currentValue = inputValue != nil ? inputValue : 0
+        }
+        
         currentValue = walkTree(operationQueue, currentValue!)
         inputString = "\(currentValue)"
         println("\(currentValue)")
@@ -110,12 +146,50 @@ class ViewController: UIViewController {
         currentOperation = nil
         inputValue = nil
         operationQueue = [Node]()
+        decimalCount = 0
+        decimalFlag = false
     }
 
     func currentFromInput() {
         if currentValue == nil {
             currentValue = inputValue
         }
+    }
+    
+    func formatInputString(input: String) {
+        if decimalFlag {
+            if decimalCount == 1 {
+                inputString += ".\(input)"
+                decimalCount++
+            } else {
+                inputString += input
+                decimalCount++
+            }
+        } else {
+            inputString += input
+        }
+    }
+    
+    
+    func formatInputValue() {
+        let aMorePowerfulString = inputString as NSString
+        inputValue = aMorePowerfulString.floatValue
+    }
+    
+    func currentValueIsDisplayed() -> Bool {
+        if currentValue != nil {
+            let cvString =  "\(currentValue)"
+            return cvString == inputString
+        }
+        return false
+    }
+    
+    func updateDisplayWithCurrent() {
+        inputString = "\(currentValue)"
+    }
+    
+    func updateDisplayWithInput(){
+        inputString = "\(inputValue)"
     }
 }
 
